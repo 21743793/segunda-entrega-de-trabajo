2b4792d0c95b6a03de5d5ee040a54ddc8ebf4541
21743793 <126355849+21743793@users.noreply.github.com>
2023-03-15 20:57:17 -0300

    Add files via upload

---
+++ js/myScript.js
@@ -0,0 +1,19 @@
+
+/**
+document.getElementById("boton").onclick = function () {
+    document.getElementById("demo").innerHTML = "Curriculum Vitae"
+}
+
+
+document.addEventListener("click", function() {
+document.getElementById("demo").innerHTML = "Curriculum Vitae" 
+
+})
+*/
+
+
+
+document.getElementById("boton").addEventListener("click", function() {
+    document.getElementById("demo").innerHTML = "Curriculum Vitae" 
+    
+    })
